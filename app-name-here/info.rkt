f37ea84355339af0b7808318064dc880ede54e82
Bogdan Popa <popa.bogdanp@gmail.com>
2019-01-26 18:50:32 +0200

    fix(build): add missing deps

--- app-name-here/info.rkt
+++ app-name-here/info.rkt
@@ -3,6 +3,7 @@
 (define collection "app-name-here")
 (define deps '("base"
                "component-lib"
-               "sql"
+               "db-lib"
+               "gregor-lib"
                "web-server-lib"))
 (define build-deps '("rackunit-lib"))
